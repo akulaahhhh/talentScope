33e0a250b98658a9ebcbbf26c84fe3b139c4145b
asjadZ <asjad@zohlgroup.com>
2024-11-27 17:21:05 +0800

    make route group for login, index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [MainController::class, 'index'])->name('index');
+
+
+
+Route::as('talentScope.')->group(function (){
+    Route::get('/', [MainController::class, 'index'])->name('index');
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+});
