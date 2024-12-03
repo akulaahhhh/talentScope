8c23113b37dcc191286b5163cc5e0a0d583776ab
jadMizan <asjadmizan@gmail.com>
2024-12-03 12:27:16 +0800

    add controller for login , register_candi and register_org

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,6 +9,15 @@ class MainController extends Controller
     public function index() {
         return view('main.index');
     }
+    public function login() {
+        return view('main.login');
+    }
+    public function register_candi() {
+        return view('main.register_candi');
+    }
+    public function register_org() {
+        return view('main.register_org');
+    }
 
     
 }
