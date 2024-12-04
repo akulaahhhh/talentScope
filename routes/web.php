38d06cc835593d36112d439c5c39a4532af225b4
asjadZ <asjad@zohlgroup.com>
2024-12-04 17:17:19 +0800

    set middleware in route

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,34 @@ Route::as('talentScope.')->group(function (){
     Route::get('/', [MainController::class, 'index'])->name('index');
     Route::get('login', [MainController::class, 'login'])->name('login');
     Route::get('register', [MainController::class, 'register_candi'])->name('register_candi');
+    Route::post('register', [AuthController::class, 'register_candi'])->name('register_submit');
     Route::get('register_org', [MainController::class, 'register_org'])->name('register_org');
+    Route::post('register_org', [AuthController::class, 'register_org'])->name('register_org_submit');
     Route::post('login', [AuthController::class, 'login']);
 
+
+
+    Route::middleware(['auth', 'OnlyAdmin'])->group(function () {
+
+        // Route::get('/admin/dashboard', 'AdminController@index');
+    
+    });
+    
+    Route::middleware(['auth', 'OnlyOrganizers'])->group(function () {
+    
+        // Route::get('/admin/dashboard', 'AdminController@index');
+    
+    });
+    
+    Route::middleware(['auth', 'OnlyCandidates'])->group(function () {
+    
+        // Route::get('/admin/dashboard', 'AdminController@index');
+    
+    });
+
+
 });
 
 
-Route::middleware(['auth', 'OnlyAdmin'])->group(function () {
 
-    // Route::get('/admin/dashboard', 'AdminController@index');
-
-});
-
-Route::middleware(['auth', 'OnlyOrganizers'])->group(function () {
-
-    // Route::get('/admin/dashboard', 'AdminController@index');
-
-});
-
-Route::middleware(['auth', 'OnlyCandidates'])->group(function () {
-
-    // Route::get('/admin/dashboard', 'AdminController@index');
-
-});
 
