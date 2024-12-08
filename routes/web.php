f32ee66fd1d033dfd9002ff0c9c94117599301ac
jadMizan <asjadmizan@gmail.com>
2024-12-08 16:28:31 +0800

    swt route or email verification

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +19,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index'])->name('index');
 
+// The Email Verification Notice
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
 
+//The Email Verification Handler
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect()->route('talentScope.login');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+// The email verification handle jugak but auto login
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
+
+//     // Automatically log in the user
+//     $user = $request->user();
+//     Auth::login($user);
+
+//     // Redirect to a desired page after login
+//     return redirect()->route('dashboard');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
+
+// RESEND EMAIL FUNCTION
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('success', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::as('talentScope.')->group(function (){
     Route::get('/', [MainController::class, 'index'])->name('index');
@@ -30,19 +61,18 @@ Route::as('talentScope.')->group(function (){
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 
-
     Route::middleware(['onlyAdmin'])->group(function () {
 
         // Route::get('/admin/dashboard', 'AdminController@index');
     
     });
     
-    Route::middleware(['onlyOrg'])->group(function () {
+    Route::middleware(['onlyOrg', 'verified'])->group(function () {
     
         // Route::get('/admin/dashboard', 'AdminController@index');
     });
     
-    Route::middleware(['onlyCandi'])->group(function () {
+    Route::middleware(['onlyCandi', 'verified'])->group(function () {
         // Route::get('/', [MainController::class, 'index'])->name('index');
     
         // Route::get('/admin/dashboard', 'AdminController@index');
