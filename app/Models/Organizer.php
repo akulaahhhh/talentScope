28f335b78a2b442ea7a9ce55a003e98c1e7e8c5f
jadMizan <asjadmizan@gmail.com>
2025-01-05 18:41:52 +0800

    make program model and relation to org

--- app/Models/Organizer.php
+++ app/Models/Organizer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Organizer extends Model
 {
@@ -23,5 +24,9 @@ class Organizer extends Model
     public function user() : BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+    public function programs(): HasMany
+    {
+        return $this->hasMany(programs::class);
     }
 }
