8a5d250e92f21f541788ead6c240908beb7da812
jadMizan <asjadmizan@gmail.com>
2025-01-05 18:42:00 +0800

    reformat

--- resources/views/components/main-navbar.blade.php
+++ resources/views/components/main-navbar.blade.php
@@ -1,22 +1,22 @@
 <div>
     <header id="header" class="header d-flex align-items-center fixed-top">
         <div class="container-fluid container-xl position-relative d-flex align-items-center">
-    
-          <a href="/" class="logo d-flex align-items-center me-auto">
-            <img src="{{ asset('talentScope-img') }}/talentScope-mentol.svg" alt="">
-            <h1 class="sitename">talentScope</h1>
-          </a>
-    
-          <nav id="navmenu" class="navmenu">
-            <ul>
-              <li><a href="/#hero" class="active">Home</a></li>
-              <li><a href="index.html#about">Programs</a></li>
-              <li><a href="index.html#features">FAQ</a></li>
-              <li><a href="index.html#services">About</a></li>
-              {{-- <li><a href="index.html#pricing">Pricing</a></li> --}}
 
-              {{-- ada dropdown --}}
-              {{-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <a href="/" class="logo d-flex align-items-center me-auto">
+                <img src="{{ asset('talentScope-img') }}/talentScope-mentol.svg" alt="">
+                <h1 class="sitename">talentScope</h1>
+            </a>
+
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                    <li><a href="/#hero" class="active">Home</a></li>
+                    <li><a href="index.html#about">Programs</a></li>
+                    <li><a href="index.html#features">FAQ</a></li>
+                    <li><a href="index.html#services">About</a></li>
+                    {{-- <li><a href="index.html#pricing">Pricing</a></li> --}}
+
+                    {{-- ada dropdown --}}
+                    {{-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
                 <ul>
                   <li><a href="#">Dropdown 1</a></li>
                   <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
@@ -33,25 +33,28 @@
                   <li><a href="#">Dropdown 4</a></li>
                 </ul>
               </li> --}}
-              @if($is_logged_in === true)
-              <li><a href="{{route('talentScope.logout')}}">Logout</a></li>
-              <a class="btn-getstarted" href="index.html#about">Hi {{$user->username}}</a>
+                    @if ($is_logged_in === true)
+                        <li><a href="{{ route('talentScope.logout') }}">Logout</a></li>
+                        @if ($user->role === 'CANDIDATES')
+                            <a class="btn-getstarted" href="index.html#about">Hi {{ $user->username }}</a>
+                        @elseif ($user->role === 'ORGANIZER')
+                            <a class="btn-getstarted" href="{{route('talentScope.organizer_dashboard.organizer_dashboard')}}">Hi {{ $user->username }}</a>
+                        @endif
+                    @else
+                        <li><a href="{{ route('talentScope.register_candi') }}">Register</a></li>
+                        <li><a href="{{ route('talentScope.login') }}">Login</a></li>
+                        <a class="btn-getstarted" href="{{ route('talentScope.register_org') }}">Post your Event!</a>
+                    @endif
 
-              @else
-              <li><a href="{{route('talentScope.register_candi')}}">Register</a></li>
-              <li><a href="{{route('talentScope.login')}}">Login</a></li>
-              <a class="btn-getstarted" href="{{route('talentScope.register_org')}}">Post your Event!</a>
-              @endif
-
-            </ul>
-            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-          </nav>
-          {{-- @if($is_logged_in === true)
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+            {{-- @if ($is_logged_in === true)
           <a class="btn-getstarted" href="index.html#about">Profile</a>
               
           @else
               nyenyey
           @endif --}}
         </div>
-      </header>
+    </header>
 </div>
