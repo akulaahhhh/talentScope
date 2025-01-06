12f7f3570c38b6add170fc7725ff605049aeca6d
jadMizan <asjadmizan@gmail.com>
2025-01-07 01:06:17 +0800

    add upload event page

--- app/Http/Controllers/OrgController.php
+++ app/Http/Controllers/OrgController.php
@@ -9,4 +9,7 @@ class OrgController extends Controller
     public function index() {
         return view('main.organizer_dashboard.index');
     }
+    public function view_upload_event() {
+        return view('main.organizer_dashboard.upload_event');
+    }
 }
