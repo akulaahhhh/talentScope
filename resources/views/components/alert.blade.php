ab8677f5098100f0b2cb2789822e393d21bda536
asjadZ <asjad@zohlgroup.com>
2025-01-09 17:12:44 +0800

    add alert when any error occur

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -29,7 +29,7 @@
                 alert.classList.add('fade');
                 alert.classList.remove('show');
             }
-        }, 10000);  // 5 seconds
+        }, 13000);  // 13 seconds
     </script>
 
 @elseif(session('fail'))
@@ -63,7 +63,46 @@
                 alert.classList.add('fade');
                 alert.classList.remove('show');
             }
-        }, 10000);  // 5 seconds
+        }, 13000);  // 13 seconds
     </script>
 @endif
 
+@if ($errors->any())
+
+                <div class="alert fail alert-danger alert-dismissible fade show" role="alert" id="alertMessageFail">
+                    <style>
+                        .fail {
+                            position: fixed;
+                            top: 90px; /* Distance from the top of the viewport */
+                            left: 50%;
+                            transform: translateX(-50%); /* Center the alert horizontally */
+                            z-index: 1050; /* Ensure it appears above other content */
+                            width: 100%; /* Optional: set width to 100% or a fixed width if needed */
+                            max-width: 500px; /* Optional: limit the width */
+                            /* background-color: #4adb6cd3; */
+                            color: rgb(0, 0, 0);
+                            padding: 10px;
+                            border-radius: 5px;
+                            text-align: center;
+                            height: 5%;
+                        }
+                    </style>
+                    <ul>
+                        @foreach ($errors->all() as $err)
+                            <li>{{ $err }}</li>
+                        @endforeach
+                    </ul>
+                    <button type="button" class="btn-close pt-1" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            
+                <script>
+                    // Automatically close the alert after 5 seconds
+                    setTimeout(function() {
+                        let alert = document.getElementById('alertMessageFail');
+                        if (alert) {
+                            alert.classList.add('fade');
+                            alert.classList.remove('show');
+                        }
+                    }, 13000);  // 13 seconds
+                </script>
+@endif
