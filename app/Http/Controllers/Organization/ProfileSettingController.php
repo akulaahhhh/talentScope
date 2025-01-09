eff61e5e7352be50d79ef6944d5b55f8170a2bfd
asjadZ <asjad@zohlgroup.com>
2025-01-09 17:11:46 +0800

    add update user info and password

--- app/Http/Controllers/Organization/ProfileSettingController.php
+++ app/Http/Controllers/Organization/ProfileSettingController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Organization;
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileSettingController extends Controller
 {
@@ -14,7 +15,7 @@ class ProfileSettingController extends Controller
         $validated = $request->validate([
             'email' => 'email|required',
             'org_name' => 'required',
-            'ssm' => 'number|required',
+            'ssm' => 'numeric|required',
             'type' => 'required',
             'desc' => 'nullable',
             'address' => 'nullable',
@@ -23,10 +24,33 @@ class ProfileSettingController extends Controller
         $user = User::find($user->id);
 
         $user->update([
-            
-        ])
+            'email' => $validated['email']
+        ]);
         $user->organizer->update([
+            'org_name' => $validated['org_name'],
+            'ssm' => $validated['ssm'],
+            'type' => $validated['type'],
+            'description' => $validated['desc'],
+            'address' => $validated['address']
+        ]);
+        return redirect()
+            ->route('talentScope.organizer_dashboard.view_profile_setting')
+            ->with('success', 'Profile setting saved succesfully.');
+    }
+    public function update_password_org(Request $request) {
 
-        ])
+        $user = $request->user;
+
+        $validated = $request->validate([
+            'password' => 'confirmed',
+        ]);
+
+        $user->update([
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return redirect()
+            ->route('talentScope.organizer_dashboard.view_profile_setting')
+            ->with('success', 'Password changed succesfully.');
     }
 }
